3f6d2d9cbbfb91635f89fd4b0373b34131a95902
jenniferceja <183955366+jenniferceja@users.noreply.github.com>
2025-06-29 17:24:08 -0500

    Login and Register
    login and register page

--- mern-app/frontend/src/pages/Register.jsx
+++ mern-app/frontend/src/pages/Register.jsx
@@ -1,3 +1,39 @@
+import { Link } from "react-router-dom"; // <-- Make sure this is imported!
+import PrimaryButton from "../components/Buttons";
+import Field from "../components/Field";
+
 export default function Register() {
-  return <h1 className="text-2xl p-4">Register Page wip...</h1>;
+  return (
+    <div className="flex flex-col items-center">
+      <br />
+      <div className="w-full max-w-sm">
+        <Field
+          label="Email"
+          name="email"
+          type="email"
+          placeholder="Example@email.com"
+          required
+        />
+        <br />
+        <Field
+          label="Password"
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+        />
+      </div>
+
+      <div className="mt-6">
+        <PrimaryButton>Sign up</PrimaryButton>
+      </div>
+
+      <p className="text-base p-4">
+        Have an account?{" "}
+        <Link to="/login" className="text-blue-500 underline hover:text-blue-700">
+          Log in
+        </Link>
+      </p>
+    </div>
+  );
 }
