80d853e18b8eaa3b4a46b4e73b46e9aea2f268ef
Rainer <136118910+RhythmNotebook@users.noreply.github.com>
2025-07-28 19:19:37 -0500

    Split part of profileController and profileController.test off into volunteerHistoryController and volunteerHistoryController.test

--- mern-app/backend/controllers/profileController.js
+++ mern-app/backend/controllers/profileController.js
@@ -43,39 +43,3 @@ export const updateProfile = async (req, res) => {
 };
 
 
-export const getHistory = async (req, res) => {
-  try {
-    const { accountId } = req.params;
-    console.log("GET /api/profile/:accountId");
-    console.log("accountId:", accountId);
-
-    const history =
-      (Object.values(mockProfiles).find((p) => p.accountId === accountId) || { eventHistory: [] }).eventHistory;
-
-
-    console.log("Returning full history:", history);
-    res.json(history);
-  } catch (err) {
-    console.error("getHistory error:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
-export const getAttendedHistory = async (req, res) => {
-  try {
-    const { accountId } = req.params;
-    console.log("GET /api/profile/:accountId");
-    console.log("accountId:", accountId);
-
-    const history =
-      (Object.values(mockProfiles).find((p) => p.accountId === accountId) || { eventHistory: [] }).eventHistory.filter(event => event.attended);
-
-
-    console.log("Returning attended history:", history);
-    res.json(history);
-  } catch (err) {
-    console.error("getHistory error:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
