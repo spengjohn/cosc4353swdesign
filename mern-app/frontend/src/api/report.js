e2177f97150bc05ac0aaee9c74f772c1950288ed
spengjohn <165310865+spengjohn@users.noreply.github.com>
2025-08-05 20:00:27 -0500

    get reports and fixes

--- mern-app/frontend/src/api/report.js
+++ mern-app/frontend/src/api/report.js
@@ -1,15 +1,18 @@
+
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
+
 export const downloadVolunteerCSV = () => {
   // Open the file download in a new tab (or same tab)
-  window.open('/api/reports/volunteers/csv', '_blank');
+  window.open(`${API_BASE}}/reports/volunteers/csv`, '_blank');
 };
 
 export const downloadVolunteerJSON = () => {
-  window.open('/api/reports/volunteers/json', '_blank');
+  window.open(`${API_BASE}}/reports/volunteers/json`, '_blank');
 }
 
 export const downloadEventCSV = () => {
-  window.open('/api/reports/events/csv', '_blank');
+  window.open(`${API_BASE}}/reports/events/csv`, '_blank');
 }
 export const downloadEventJSON = () => {
-  window.open('/api/reports/events/json', '_blank');
+  window.open(`${API_BASE}}/reports/events/json`, '_blank');
 }
