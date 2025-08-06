00387cc721ca4b9f6c57526e966126bdb7704c25
spengjohn <165310865+spengjohn@users.noreply.github.com>
2025-08-05 20:04:37 -0500

    typo fix

--- mern-app/frontend/src/api/report.js
+++ mern-app/frontend/src/api/report.js
@@ -3,16 +3,16 @@ const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 export const downloadVolunteerCSV = () => {
   // Open the file download in a new tab (or same tab)
-  window.open(`${API_BASE}}/reports/volunteers/csv`, '_blank');
+  window.open(`${API_BASE}/reports/volunteers/csv`, '_blank');
 };
 
 export const downloadVolunteerJSON = () => {
-  window.open(`${API_BASE}}/reports/volunteers/json`, '_blank');
+  window.open(`${API_BASE}/reports/volunteers/json`, '_blank');
 }
 
 export const downloadEventCSV = () => {
-  window.open(`${API_BASE}}/reports/events/csv`, '_blank');
+  window.open(`${API_BASE}/reports/events/csv`, '_blank');
 }
 export const downloadEventJSON = () => {
-  window.open(`${API_BASE}}/reports/events/json`, '_blank');
+  window.open(`${API_BASE}/reports/events/json`, '_blank');
 }
